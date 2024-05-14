0d86cf84317b3645ca47350fedbc68d910b3774f
Yao Zi <ziyao@disroot.org>
2024-05-15 02:55:30 +0800

    include/memory: rename memory macros

--- include/za/memory.h
+++ include/za/memory.h
@@ -1,13 +1,22 @@
+/*
+ *	libza
+ *	/include/za/memory.h
+ *	SPDX-License-Identifier: MPL-2.0
+ *	Copyright (c) 2024 Yao Zi <ziyao@disroot.org>
+ */
+
 #ifndef __ZA_IO_H_INC__
 #define __ZA_IO_H_INC__
 
 #include<stdint.h>
 
-#define writeb(addr, b)		*(volatile uint8_t *)(addr) = (b)
-#define readb(addr)		*(volatile uint8_t *)(addr)
-#define writes(addr, s)		*(volatile uint16_t *)(addr) = (s)
-#define reads(addr)		*(volatile uint16_t *)(addr)
-#define writel(addr, l)		*(volatile uint32_t *)(addr) = (l)
-#define readl(addr)		*(volatile uint32_t *)(addr)
+#define za_iomem		volatile
+
+#define za_writeb(addr, b)	*(volatile uint8_t *)(addr) = (b)
+#define za_readb(addr)		*(volatile uint8_t *)(addr)
+#define za_writes(addr, s)	*(volatile uint16_t *)(addr) = (s)
+#define za_reads(addr)		*(volatile uint16_t *)(addr)
+#define za_writel(addr, l)	*(volatile uint32_t *)(addr) = (l)
+#define za_readl(addr)		*(volatile uint32_t *)(addr)
 
 #endif	// __ZA_IO_H_INC__
