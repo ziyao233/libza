f885a2cd13d559e12b688323e2b7d1a0e6bb3abd
Yao Zi <ziyao@disroot.org>
2024-05-16 03:53:18 +0800

    include: make za_write/za_read macros more robust

--- include/za/memory.h
+++ include/za/memory.h
@@ -12,11 +12,11 @@
 
 #define za_iomem		volatile
 
-#define za_writeb(addr, b)	*(volatile uint8_t *)(addr) = (b)
-#define za_readb(addr)		*(volatile uint8_t *)(addr)
-#define za_writes(addr, s)	*(volatile uint16_t *)(addr) = (s)
-#define za_reads(addr)		*(volatile uint16_t *)(addr)
-#define za_writel(addr, l)	*(volatile uint32_t *)(addr) = (l)
-#define za_readl(addr)		*(volatile uint32_t *)(addr)
+#define za_writeb(addr, b)	(*(volatile uint8_t *)(addr)) = (b)
+#define za_readb(addr)		(*(volatile uint8_t *)(addr))
+#define za_writes(addr, s)	(*(volatile uint16_t *)(addr)) = (s)
+#define za_reads(addr)		(*(volatile uint16_t *)(addr))
+#define za_writel(addr, l)	(*(volatile uint32_t *)(addr)) = (l)
+#define za_readl(addr)		(*(volatile uint32_t *)(addr))
 
 #endif	// __ZA_IO_H_INC__
